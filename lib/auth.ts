ad506b10ccfe9232465b9c5d3289723f5ac233bd
Rahul2271 <rraahhuullch@gmail.com>
2026-05-22 16:56:24 +0530

    s

--- lib/auth.ts
+++ lib/auth.ts
@@ -34,11 +34,17 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async jwt({ token, user }) {
-      if (user) { token.id = user.id; token.role = (user as { role: string }).role }
+      if (user) {
+        token.id = user.id
+        token.role = (user as unknown as { role: string }).role
+      }
       return token
     },
     async session({ session, token }) {
-      if (token) { session.user.id = token.id as string; session.user.role = token.role as string }
+      if (token) {
+        session.user.id   = token.id as string
+        session.user.role = token.role as string
+      }
       return session
     },
   },
